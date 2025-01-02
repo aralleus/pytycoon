fb489693633952cbab9a24f8f69783576500fd4e
aralleus <sayrex815@gmail.com>
2025-01-02 20:49:48 +0200

    i forgot to update it.

--- menu/menu.py
+++ menu/menu.py
@@ -20,30 +20,38 @@ class menu:
 
     def display(self, actions, descriptions, curIndex):
         cls()
+        output = []
 
         logo = r"""
-                             __
-                            /\ \__
-         _____    __  __    \ \ ,_\   __  __      ___     ___     ___     ___
-        /\ '__`\ /\ \/\ \    \ \ \/  /\ \/\ \    /'___\  / __`\  / __`\ /' _ `\
-        \ \ \ \ \\ \ \_\ \    \ \ \_ \ \ \_\ \  /\ \__/ /\ \ \ \/\ \ \ \/\ \/\ \
-         \ \ ,__/ \/`____ \    \ \__\ \/`____ \ \ \____\\ \____/\ \____/\ \_\ \_\
-          \ \ \/   `/___/> \    \/__/  `/___/> \ \/____/ \/___/  \/___/  \/_/\/_/
-           \ \_\      /\___/              /\___/
-            \/_/      \/__/               \/__/ 
-        """
-        print(colored(center_block(logo), "blue"))
+                                     __
+                                    /\ \__
+                 _____    __  __    \ \ ,_\   __  __      ___     ___     ___     ___
+                /\ '__`\ /\ \/\ \    \ \ \/  /\ \/\ \    /'___\  / __`\  / __`\ /' _ `\
+                \ \ \ \ \\ \ \_\ \    \ \ \_ \ \ \_\ \  /\ \__/ /\ \ \ \/\ \ \ \/\ \/\ \
+                 \ \ ,__/ \/`____ \    \ \__\ \/`____ \ \ \____\\ \____/\ \____/\ \_\ \_\
+                  \ \ \/   `/___/> \    \/__/  `/___/> \ \/____/ \/___/  \/___/  \/_/\/_/
+                   \ \_\      /\___/              /\___/
+                    \/_/      \/__/               \/__/ 
+                """
+
+        print(colored(center_block(logo, anchor="center"), "blue"))
+        print(' ')
 
         for i, action in enumerate(actions):
             if i == curIndex:
                 if descriptions and descriptions[i]:
-                    print(center(f"> {action} - {descriptions[i]}"))
+                    line = str(center(f"> {action} - {descriptions[i]}", anchor="center"))
                 else:
-                    print(center(f"> {action}"))
+                    line = str(center(f"> {action}", anchor="center"))
             else:
-                print(center(colored(f"  {action}", "blue")))
+                line = str(center(colored(f"  {action}", "blue"), anchor="center"))
+
+            output.append(line)
+        print("\n".join(output))
 
     def check(self):
+        from menu.scenes.mloader import mloader
+        mloader = mloader()
         self.display(self.actions, self.descriptions, self.curIndex)
 
         while True:
@@ -64,11 +72,14 @@ class menu:
                 elif self.curIndex == 1:
                     pass
                 elif self.curIndex == 2:
-                    pass
+                    mloader.display_mods()
+                    break
                 elif self.curIndex == 3:
+                    pass    
+                elif self.curIndex == 4:
                     about.start()
                     break
-                elif self.curIndex == 4:
+                elif self.curIndex == 5:
                     cls()
                     quit()
 
